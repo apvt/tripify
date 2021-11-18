a17baebe74649a4e008393e30d98881ecf58e824
r <r@localhost.localdomain>
2021-11-18 19:54:51 +0530

    added basic of nav and footer

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const menu_btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+
+menu_btn.addEventListener('click',()=>{
+    menu.classList.toggle('show');
+});
